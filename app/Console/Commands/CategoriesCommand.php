df4de82455b9bea551b8054e0fff593cef8927f8
Manu Ortega <mortega@indigenasdigitales.com>
2024-03-02 22:46:37 +0100

    feat - Adding command for categories table - command: php artisan insert:categories

---
+++ app/Console/Commands/CategoriesCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class CategoriesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'insert:categories';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Insert categories from JSON';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Get JSON
+        $json = Storage::get('categories.json');
+
+        // Decode JSON
+        $data = json_decode($json, true);
+
+        // Root category
+        $root = Category::create([
+            'name' => 'root',
+            'short_code' => null,
+            'description' => null,
+            'parent_id' => null,
+        ]);
+
+        // Parent categories
+        foreach ($data as $category) {
+            $parent = Category::create([
+                'name' => $category['short_code'],
+                'short_code' => $category['short_code'],
+                'description' => $category['topic'],
+                'parent_id' => $root->id,
+            ]);
+
+            // Subcategories
+            foreach ($category['subtopics'] as $subtopic) {
+                Category::create([
+                    'name' => $subtopic['short_code'],
+                    'short_code' => $subtopic['short_code'],
+                    'description' => $subtopic['topic'],
+                    'parent_id' => $parent->id,
+                ]);
+            }
+        }
+
+        $this->info('Categories inserted successfully.');
+    }
+}
