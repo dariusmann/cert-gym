1de56a5335466c217b020c34852c4034710335b2
bilal rasool <478bilal@gmail.com>
2024-03-11 17:38:29 +0500

    worked on second TODO, add the category path in the resposen of question get by id

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -27,12 +27,32 @@ class Question extends Model implements JsonSerializable
         return $this->hasMany(QuestionAnswer::class);
     }
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    private function getCategoryHierarchy($category, $hierarchy = []): array
+{
+    if (!$category) {
+        return array_reverse($hierarchy);
+    }
+
+    $hierarchy[] = [
+        'id' => $category->id,
+        'name' => $category->name,
+    ];
+
+    return $this->getCategoryHierarchy($category->parent, $hierarchy);
+}
+
     public function jsonSerialize(): mixed
     {
         return [
             'id' => $this->getId(),
             'text' => $this->text,
             'category_id' => $this->category_id,
+            'category_path' => $this->getCategoryHierarchy($this->category),
             'answers' => $this->answers()->get(),
         ];
     }
