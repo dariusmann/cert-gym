227c25aec9d10fb53f038fae20000c6782a74826
bilal rasool <478bilal@gmail.com>
2024-03-15 14:45:39 +0500

    update the mapping array of categories and alos update the logic to calculate the readiness scrore

--- app/Http/Controllers/Tracking/ReadReadinessScoreController.php
+++ app/Http/Controllers/Tracking/ReadReadinessScoreController.php
@@ -20,68 +20,69 @@ class ReadReadinessScoreController extends Controller
         /** @var User $user */
         $user = $request->user();
 
-
-        $countMapping = [
-            2 => 12,   // Parent category ID => Number of questions
-            9 => 6,    // Parent category ID => Number of questions
-            13 => 24,  // Parent category ID => Number of questions
-            26 => 12,  // Parent category ID => Number of questions
-            33 => 10,  // Parent category ID => Number of questions
-            39 => 11   // Parent category ID => Number of questions
-        ];
-        
-     
-        
         $totalReadiness = 0;
-        $categoryCount = count($countMapping);
-        
-        foreach ($countMapping as $parentCategoryId => $questionCount) {
-            // Find subcategories of the parent category
-            $subcategories = Category::where('parent_id', $parentCategoryId)->pluck('id');
-        
+        $rootTaskListCategory = Category::where('short_code', 'tl-root')->first();
+        $mainCategories = Category::where('parent_id', $rootTaskListCategory->getId())->get();
+
+        foreach ($mainCategories as $mainCategory) {
             $subcategoryReadiness = 0;
-        
-            // Calculate readiness for each subcategory
-            foreach ($subcategories as $subcategoryId) {
-                // Get the most recent question attempts for the subcategory
-                $questionIds = Question::where('category_id', $subcategoryId)->pluck('id');
-                
-                $recentQuestionAttempts = QuestionAttempt::whereIn('question_id', $questionIds)
-                    ->where('user_id', $user->getId())
-                    ->orderByDesc('created_at')
-                    ->take($questionCount)
-                    ->get();
-        
-                // Count the total number of questions attempted
-                $totalQuestions = $recentQuestionAttempts->count();
-        
-                // Count the total number of correct answers
-                $totalCorrectAnswers = $recentQuestionAttempts->where('answered_correctly', true)->count();
-        
-                // Calculate readiness for the subcategory
-                $subcategoryReadiness += $totalQuestions > 0 ? ( $totalCorrectAnswers / $totalQuestions) * 100 : 0;
-                
-                
-            }
-        
-            // Calculate average readiness for the parent category
-            $parentCategoryReadiness = $subcategories->count() > 0 ? $subcategoryReadiness / $subcategories->count() : 0;
-        
-            // Add parent category readiness to total readiness
-            $totalReadiness += $parentCategoryReadiness;
-        }
-        
-        // Calculate average readiness across all parent categories
-        $averageReadiness = $categoryCount > 0 ? $totalReadiness / $categoryCount : 0;
 
+            $subcategories = Category::where('parent_id', $mainCategory->id)->pluck('id');
+        
+            // Get the most recent question attempts for the subcategory
+            $questionIds = Question::whereIn('category_id', $subcategories)->pluck('id');
 
-
+            $count = $this->getQuestionCountAgainstEachMainCategory($mainCategory->short_code);
+            
+            $recentQuestionAttempts = QuestionAttempt::whereIn('question_id', $questionIds)
+                ->where('user_id', $user->getId())
+                ->orderByDesc('created_at')
+                ->take($count)
+                ->get();
+    
+            // Count the total number of questions attempted
+            $totalQuestions = $recentQuestionAttempts->count();
+    
+            // Count the total number of correct answers
+            $totalCorrectAnswers = $recentQuestionAttempts->where('answered_correctly', true)->count();
+    
+            // Calculate readiness for the subcategory
+            $subcategoryReadiness = $totalQuestions > 0 ? ( $totalCorrectAnswers / $totalQuestions) * 100 : 0;
        
+           
+            // Add parent category readiness to total readiness
+            $totalReadiness += $subcategoryReadiness;
+
+        } 
+        
+          // Calculate average readiness across all parent categories
+          $averageReadiness = $mainCategories->count() > 0 ? $totalReadiness / $mainCategories->count() : 0;
+      
+      
+      
+      
+      
         return new JsonResponse([
           
             'ReadinessScore' => round($averageReadiness)
         ]);
 
+    }
+
+    private function getQuestionCountAgainstEachMainCategory($short_code)
+    {
+        // Define the counts of questions for each main category
+        $countMapping = [
+            'A' => 12, // 12  questions from subcategories of the first category
+            'B' => 6,  // 6   questions from subcategories of the second category
+            'C' => 24, // 24  questions from subcategories of the third category
+            'D' => 12, // 12  questions from subcategories of the fourth category
+            'E' => 10, // 10  questions from subcategories of the fifth category
+            'F' => 11  // 11  questions from subcategories of the sixth category
+        ];
+
+        // Return the count based on the category ID, default to 0 if not found
+        return $countMapping[$short_code] ?? 0;
     }
 
    
